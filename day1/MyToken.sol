a5587143c65c55c513b8e11d81d18ebbdfac1f0d
Frank Kong <frankkongnj@gmail.com>
2024-01-13 18:20:23 +0800

    upate metadata from ruth to shibainu

--- day1/MyToken.sol
+++ day1/MyToken.sol
@@ -8,7 +8,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contr
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract MyToken is ERC721URIStorage, Ownable {
-    string constant public TOKEN_URI = "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
+    string constant public TOKEN_URI = "ipfs://QmXw7TEAJWKjKifvLE25Z9yjvowWk2NWY3WgnZPUto9XoA";
     uint256 counter;
     uint256 MINIMUM_USD = 500 * 10 ** 18;
     AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
--- day2/MyNFT.sol
+++ day2/MyNFT.sol
@@ -7,7 +7,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MyToken is ERC721URIStorage, Ownable {
-    string constant public TOKEN_URI = "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
+    string constant public TOKEN_URI = "ipfs://QmXw7TEAJWKjKifvLE25Z9yjvowWk2NWY3WgnZPUto9XoA";
     uint256 counter;
 
     constructor() ERC721("MyNFT", "MN") {}
